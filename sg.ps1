6936c9b33de42c585b67c00d04177e6582ab2ff6
karthikkaku <128590795+karthikkaku@users.noreply.github.com>
2024-01-10 20:55:51 +0530

    Update sg.ps1

--- sg.ps1
+++ sg.ps1
@@ -1,7 +1,7 @@
 $accessKey = "AKIAY7SEYN2PGNMGEEFK"
 $secretKey = "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"
 $region = "us-east-2"
-$securityGroupId = "sg-06d20f1c94cf9b730"
+$securityGroupId =  $env:sg-id
 
 # Set the security group details
 $protocol = "tcp"
